7242a3e0894550bece3c09b003cb8e410ff331a1
Felipe Valentim <felipe@practicesoftware.com.br>
2024-11-15 16:30:31 -0300

    Updates

--- assets/Login-DSksZYEn.js
+++
@@ -1,1 +0,0 @@
-import{j as o,L as t}from"./index-Bj_QrwHo.js";const r=()=>o.jsx(t,{to:"/Home",children:"Home"});export{r as default};
---
+++ assets/Login-D_g4IkuD.js
@@ -0,0 +1,1 @@
+import{j as o,L as t}from"./index-DPEbnyZm.js";const s=()=>o.jsx(t,{to:"/",children:"Home"});export{s as default};
